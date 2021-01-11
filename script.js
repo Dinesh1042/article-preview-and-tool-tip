dd670765d2225e0c92bb53c948a146dd0e55d298
Dinesh <70156399+Dinesh1042@users.noreply.github.com>
2021-01-11 11:45:09 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const shareEl = document.getElementById("share");
+const toolTipContEl = document.getElementById("toolTipCont");
+const toolTip = document.getElementById("toolTip");
+const shareIconEL = document.getElementById("shareIcon");
+const bottomRightEl = document.getElementById("bottomRight");
+
+function tooTipPosition() {
+  toolTipContEl.style.left =
+    shareEl.offsetLeft - toolTip.offsetWidth / 2 + 11 + "px";
+  toolTipContEl.style.top = shareEl.offsetTop - 60 + "px";
+}
+
+tooTipPosition();
+
+window.addEventListener("resize", tooTipPosition);
+
+shareEl.addEventListener("click", (e) => {
+  bottomRightEl.classList.toggle("active");
+  tooTipPosition();
+});
+
+shareIconEL.addEventListener("click", (e) => {
+  bottomRightEl.classList.toggle("active");
+  tooTipPosition();
+});
+
+// Outside click tool tip display none
+
+window.addEventListener("click", (e) => {
+  if (
+    !e.target.parentElement.closest("#toolTipCont") &&
+    !e.target.parentElement.closest(".share")
+  ) {
+    bottomRightEl.classList.remove("active");
+  }
+});
